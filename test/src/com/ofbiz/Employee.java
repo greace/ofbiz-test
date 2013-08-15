cfbe6c5f21fc1030a5c38486b431ed6edeb0bb2f
lihuan <Administrator@PC2013081314kal>
2013-08-15 12:07:53 +0800

    啊啊

--- test/src/com/ofbiz/Employee.java
+++ test/src/com/ofbiz/Employee.java
@@ -6,6 +6,13 @@ public class Employee {
 	private Integer age;
 	private String password;
 	
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 	public Long getId() {
 		return id;
 	}
