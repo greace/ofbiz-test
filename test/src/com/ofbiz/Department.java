4c0da59c1b9f2ab58167653d19a3bc1cd417bc30
lihuan <Administrator@PC2013081314kal>
2013-08-15 12:57:05 +0800

    添加Department模型

---
+++ test/src/com/ofbiz/Department.java
@@ -0,0 +1,21 @@
+package com.ofbiz;
+
+public class Department {
+	private Long id;
+	private String name;
+	private Employee employee;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
